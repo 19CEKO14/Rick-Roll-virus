bee760c6b8789bb1aa7085104418c0f64dccc911
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2021-10-24 17:39:17 +0200

    Added Rick Roll.bat

---
+++ Rick Roll.bat
@@ -0,0 +1,27 @@
+@echo off
+start
+https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+shutdown -s -t 213 -c "Never gonna give you up, never gonna let you down! You've just been Rick Rolled!
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
