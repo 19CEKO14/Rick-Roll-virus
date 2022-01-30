fd354c8aa6d6f2c782192e42239c3263409bda13
19CEKO14 <98694989+19CEKO14@users.noreply.github.com>
2022-01-30 15:02:58 +0200

    Update Rick Roll.bat

--- Rick Roll.bat
+++ Rick Roll.bat
@@ -1,7 +1,7 @@
 @echo off
 start
 https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
-shutdown -s -t 213 -c "Never gonna give you up, never gonna let you down! You've just been Rick Rolled!
+shutdown -s -t 213 -c "Never gonna give you up, never gonna let you down! Ebahta v ustata XD!
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
